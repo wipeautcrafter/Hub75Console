eb07ff52c15ac8308db8e9f1f932c7888849a445
Ronald Moesbergen <github@rmoesbergen.nl>
2019-12-13 19:35:10 +0100

    Don't trash vars in sleep

--- lib/sleep.asm
+++ lib/sleep.asm
@@ -1,5 +1,7 @@
 ; delay with r16 as parameter
 delay:
+  push r24
+  push r25
   clr r24
   clr r25
 wait_1:
@@ -10,4 +12,6 @@ wait_1:
   brne wait_1
   dec r16
   brne delay
+  pop r25
+  pop r24
   ret
